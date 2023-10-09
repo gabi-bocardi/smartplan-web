7a7f746c2e20548e6cc256c37f3e6c44e2d2f8c4
Vlad-Vekslyer <bocardigm@gmail.com>
2023-10-08 22:32:39 -0700

    Linting is working and chakra is set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,21 @@
 module.exports = {
-    extends: [
-        'eslint:recommended', 
-        'plugin:@typescript-eslint/recommended',
-        '@polyfill/eslint-config/react',
-        '@polyfill/eslint-config/node',
-
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
+  extends: [
+    '@polyfillhq/eslint-config/react',
+    'eslint:recommended',
+    'plugin:import/typescript',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
     },
-    plugins: [
-        '@typescript-eslint'
-    ],
-    rules:{
-        'react-hooks/exhaustive-deps': 'warn',
-        'no-plusplus': 'warn',
-        'react/no-unstable-nested-components': 'warn',
-        'array-callback-return': 'warn',
-        'react/no-array-index-key': 'warn',
-        'eslint-comments/no-unlimited-disable': 'warn',
-        'react/no-unused-prop-types': 'warn',
-        '@typescript-eslint/indent': 'warn',
-        'react/jsx-no-constructed-context-values': 'warn',
-        'import/no-unresolved': 'warn',
-        'global-require': 'warn',
-        '@typescript-eslint/no-floating-promises': 'warn',
-        'no-restricted-globals': 'warn',
-        'implicit-arrow-linebreak': 'warn',
-        'prefer-regex-literals': 'warn',
-        'import/no-extraneous-dependencies': 'warn',
-        'operator-linebreak': 'warn',
-        },
+  },
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
 
-    root: true,
-  };
+  root: true,
+};
